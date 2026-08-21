61f9c8b29a581bd29db40f89d08a1ec1e6b49b35
noimpty <431776604@qq.com>
2026-08-21 23:09:22 +0800

    更改异常抛出

--- service-pilot-server/src/main/java/com/servicepilot/config/SecurityConfig.java
+++ service-pilot-server/src/main/java/com/servicepilot/config/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
